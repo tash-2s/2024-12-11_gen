2d92d8c195456466a4a80ba2f98f9bfb56c7e62c
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-12-11 15:34:24 -0800

    works?

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
   const [url, setUrl] = useState("");
   const [isUrlLoading, setIsUrlLoading] = useState(false)
   const [summary, setSummary] = useState(null)
-
-  const [videoId, setVideoId] = useState(null)
-  const [isGeneratingVideo, setIsGeneratingVideo] = useState(false)
-
-  const [videoUrl, setVideoUrl] = useState(null)
 
   const foo = async () => {
     setIsUrlLoading(true)
@@ -17,33 +12,18 @@ function App() {
   }
 
   const genVid = async () => {
-    setIsGeneratingVideo(true)
-    const v = await generateVideo(summary)
-    setVideoId(v)
+    if (domManipulationArea.current) {
+      const targetDiv = domManipulationArea.current;
+
+      // <video id="avatarVideo" autoplay playsinline width="640" height="360"></video>
+      videoElement = document.createElement('p');
+      targetDiv.appendChild(videoElement);
+
+      await handleSpeak(summary)
+    }
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (videoId === null) {
-        return
-      }
-      if (videoUrl !== null) {
-        return
-      }
-
-      const u = await getVideoUrl(videoId)
-
-      if (u === null) {
-        return
-      }
-
-      setVideoUrl(u)
-    }
-
-    const interval = setInterval(fetchData, 10000)
-
-    return () => clearInterval(interval)
-  }, [])
+  const domManipulationArea = useRef(null);
 
   return (
     <>
@@ -82,14 +62,11 @@ function App() {
             type="button"
             onClick={() => genVid()}
           >
-            Generate Video
+            HeyGen!
           </button>
           <br />
           <br />
-          {isGeneratingVideo ? <div>Generating video... ({videoId})</div> : <></>}
-          <br />
-          <br />
-          {videoUrl !== null ? <VideoPlayer url={videoUrl} /> : <></>}
+          <div ref={domManipulationArea}></div>
         </>
       }
     </>
@@ -111,41 +88,6 @@ const transcribe = async (url) => {
   return r.summary
 }
 
-const generateVideo = async (text) => {
-  const r = await fetch(
-    "http://127.0.0.1:8000/heygen",
-    {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text: text }),
-    }
-  ).then(r => r.json())
-
-  return r.video_id
-}
-
-const getVideoUrl = async (videoId) => {
-  const r = await fetch(
-    `http://127.0.0.1:8000/heygen/${videoId}`,
-    {
-      method: "GET",
-    }
-  ).then(r => r.json())
-
-  return r.video_url
-}
-
-function VideoPlayer({ url }) {
-  return (
-    <div>
-      <video controls width="640" height="360">
-        <source src={url} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  );
-}
-
 const YoutubeEmbed = ({ yurl }) => {
   const embedId = yurl.split('?v=')[1]
   return <iframe
@@ -159,7 +101,7 @@ const YoutubeEmbed = ({ yurl }) => {
     />
 }
 
-// TODO: startButton, videoElement, userInput.value
+let videoElement = null
 
 import StreamingAvatar, {
   AvatarQuality,
@@ -197,8 +139,6 @@ async function initializeAvatarSession() {
 
   console.log("Session data:", sessionData);
 
-  startButton.disabled = true;
-
   avatar.on(StreamingEvents.STREAM_READY, handleStreamReady);
   avatar.on(StreamingEvents.STREAM_DISCONNECTED, handleStreamDisconnected);
 }
@@ -221,19 +161,15 @@ function handleStreamDisconnected() {
   if (videoElement) {
     videoElement.srcObject = null;
   }
-
-  startButton.disabled = false;
 }
 
-// TODO: startボタンで発火
 // Handle speaking event
-async function handleSpeak() {
+async function handleSpeak(inputValue) {
   await initializeAvatarSession()
-  if (avatar && userInput.value) {
+  if (avatar && inputValue) {
     await avatar.speak({
-      text: userInput.value,
+      text: inputValue,
       taskType: TaskType.REPEAT,
     });
-    userInput.value = ""; // Clear input after speaking
   }
 }
