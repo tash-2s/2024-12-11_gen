e2bbf33eac0e3b89ce450c94e6d516536c307977
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-12-11 10:54:38 -0800

    back: python test-heygen.py | jq . > tmp.json

--- back/test-heygen.py
+++ back/test-heygen.py
@@ -1,13 +1,38 @@
 import os
 import requests
 
-url = "https://api.heygen.com/v1/voice.list"
+# url = "https://api.heygen.com/v1/voice.list"
+#
+# headers = {
+#     "accept": "application/json",
+#     "x-api-key": os.environ["HEYGEN_API_KEY"],
+# }
+#
+# response = requests.get(url, headers=headers)
+#
+# print(response.text)
+
+url = "https://api.heygen.com/v2/video/generate"
+
+payload = {
+    "video_inputs": [{ "voice": {
+        "type": "text",
+        "input_text": "Welcome to the HeyGen API!",
+        "voice_id": "1bd001e7e50f421d891986aad5158bc8",
+    }}],
+    "caption": True,
+    "dimension": {
+        "width": 1280,
+        "height": 720,
+    },
+}
 
 headers = {
     "accept": "application/json",
+    "content-type": "application/json",
     "x-api-key": os.environ["HEYGEN_API_KEY"],
 }
 
-response = requests.get(url, headers=headers)
+response = requests.post(url, json=payload, headers=headers)
 
 print(response.text)
