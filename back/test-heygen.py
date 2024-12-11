a3f87fd061638111aed62defcf46c99060df6960
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-12-11 11:33:22 -0800

    back: found good settings

--- back/test-heygen.py
+++ back/test-heygen.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def get():
-    url = "https://api.heygen.com/v1/video_status.get?video_id=" + "4cc70043ce9a4aa29e1618b205a8ccad"
+    url = "https://api.heygen.com/v1/video_status.get?video_id=" + "2d88e7eaba4d428eaebf2428be2612f3"
 
     headers = {
         "accept": "application/json",
@@ -12,6 +12,7 @@ def get():
     response = requests.get(url, headers=headers)
 
     print(response.text)
+    print(response.json()['data']['video_url_caption']) # None or url
 
 def post():
     url = "https://api.heygen.com/v2/video/generate"
@@ -23,20 +24,26 @@ def post():
     }
 
     payload = {
-        "video_inputs": [{ "voice": {
-            "type": "text",
-            "input_text": "Welcome to the HeyGen API!",
-            "voice_id": "1bd001e7e50f421d891986aad5158bc8",
-        }}],
+        "video_inputs": [{
+            "voice": {
+                "type": "text",
+                "input_text": "Welcome to the HeyGen API!",
+                "voice_id": "d36f8d1c27054f7baa52c216ad874d16",
+            },
+            "character": {
+                "type": "avatar",
+                "avatar_id": "nik_blue_expressive_20240910",
+            },
+        }],
         "caption": True,
         "dimension": {
-            "width": 1280,
-            "height": 720,
+            "width": 720,
+            "height": 480,
         },
     }
 
     response = requests.post(url, json=payload, headers=headers)
 
-    print(response.text)
+    print(response.json()['data']['video_id'])
 
 get()
