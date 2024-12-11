d4416c7a93a8e3643c894740f010e655d195ab9c
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-12-11 10:58:06 -0800

    back: refactor script

--- back/test-heygen.py
+++ back/test-heygen.py
@@ -1,38 +1,40 @@
 import os
 import requests
 
-# url = "https://api.heygen.com/v1/voice.list"
-#
-# headers = {
-#     "accept": "application/json",
-#     "x-api-key": os.environ["HEYGEN_API_KEY"],
-# }
-#
-# response = requests.get(url, headers=headers)
-#
-# print(response.text)
+def get():
+    url = "https://api.heygen.com/v1/voice.list"
 
-url = "https://api.heygen.com/v2/video/generate"
+    headers = {
+        "accept": "application/json",
+        "x-api-key": os.environ["HEYGEN_API_KEY"],
+    }
 
-payload = {
-    "video_inputs": [{ "voice": {
-        "type": "text",
-        "input_text": "Welcome to the HeyGen API!",
-        "voice_id": "1bd001e7e50f421d891986aad5158bc8",
-    }}],
-    "caption": True,
-    "dimension": {
-        "width": 1280,
-        "height": 720,
-    },
-}
+    response = requests.get(url, headers=headers)
 
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "x-api-key": os.environ["HEYGEN_API_KEY"],
-}
+    print(response.text)
 
-response = requests.post(url, json=payload, headers=headers)
+def post():
+    url = "https://api.heygen.com/v2/video/generate"
 
-print(response.text)
+    payload = {
+        "video_inputs": [{ "voice": {
+            "type": "text",
+            "input_text": "Welcome to the HeyGen API!",
+            "voice_id": "1bd001e7e50f421d891986aad5158bc8",
+        }}],
+        "caption": True,
+        "dimension": {
+            "width": 1280,
+            "height": 720,
+        },
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "x-api-key": os.environ["HEYGEN_API_KEY"],
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
