fba4f96fd866adb91dee3eb5509d0a41f9557a64
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-12-11 10:32:58 -0800

    test heygen api access

---
+++ back/test-heygen.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+url = "https://api.heygen.com/v2/avatars"
+
+headers = {
+    "accept": "application/json",
+    "x-api-key": os.environ["HEYGEN_API_KEY"],
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
